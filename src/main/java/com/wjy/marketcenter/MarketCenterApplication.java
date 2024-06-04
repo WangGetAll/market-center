cae0a219763814c538c4c0032f7349977195629f
wangjiayu <wangdequan1126@gmail.com>
2024-06-04 20:11:36 +0800

    feat: init project

---
+++ src/main/java/com/wjy/marketcenter/MarketCenterApplication.java
@@ -0,0 +1,13 @@
+package com.wjy.marketcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjy/marketcenter/MarketCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjy.marketcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
